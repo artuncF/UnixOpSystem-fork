8e1dd6229dbdde51e27c3d2d8129e4788e66bc6c
furkan <fuartunc@gmail.com>
2016-04-17 02:11:02 +0300

    Unix OpSystem basic fork handling

---
+++ fork.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+
+int main()
+{
+    int layer=0;
+    int right_child;
+    int	left_child;
+	while((layer++)<=3)
+	{
+		left_child=fork();
+		if(left_child< 0)
+		{
+            printf("Error occured while process was being produced. Getting out in a second...");
+            sleep(1);
+            exit(-1);
+		}
+		else if (left_child== 0)
+		{
+			printf("Sol cocuk katman: %d, PID: %d, Annem: %d\n", layer-1, getpid(), getppid());
+			break;
+		}
+		else right_child=fork();
+
+		if(right_child < 0)
+		{
+            printf("Error occured while process was being produced. Getting out in a second...");
+            sleep(1);
+            exit(-1);
+		}
+		else if (right_child == 0)
+		{
+			if(layer-1==3) printf("Sag cocuk katman: %d, PID: %d, Annem: %d\n", layer-1, getpid(), getppid());
+			continue;
+		}
+		else
+		{
+			printf("Anne katman: %d, PID: %d, Sag Cocuk: %d, Sol Cocuk: %d\n", layer-1, getpid(),right_child,left_child);
+			wait(NULL);
+			wait(NULL);   			break;
+		}
+	}
+	return 0;
+}
+
